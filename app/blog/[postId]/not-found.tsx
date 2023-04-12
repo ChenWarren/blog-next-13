6eeeabc85e0e78eac532e86017133cb3be662fc8
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-04-11 20:49:25 -0700

    add metadata function in [postId] page

---
+++ app/blog/[postId]/not-found.tsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+  return (
+    <h1>The post is not found.</h1>
+  )
+}
--- app/blog/[postId]/page.tsx
+++ app/blog/[postId]/page.tsx
@@ -1,7 +1,29 @@
+import { getSortedPostsData } from '@/lib/posts'
 import { notFound } from 'next/navigation'
+
+export function generateMetadata({params}: {params: {postId: string}}) {
+    const { postId } = params
+    const posts: BlogPost[] = getSortedPostsData()
+
+    const post = posts.find( post => post.id === postId)
+
+    if(!post) return {
+        title: 'Post Not Found',
+    }
+
+    return {
+        title: post.title,
+        description: post.title,
+    }
+}
 
 export default function Post({params}: { params: {postId: string}}) {
     const { postId } = params
+    const posts: BlogPost[] = getSortedPostsData()
+
+    if(!posts.find( post => post.id === postId)) {
+        return notFound()
+    }
     return (
         <div>{postId}</div>
     )
